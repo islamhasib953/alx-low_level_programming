ce3009b319d0d6a5ba8e8758502bdec0312e2759
islamhasib953 <islamhasib953@gmail.com>
2023-09-10 18:05:33 +0300

    m

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print 0, 1, -9
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int x = 0;
+
+	while (x <= -9)
+	{
+		putchar(x + 48);
+
+		if (x != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		x++;
+	}
+	putchar('\n');
+
+	return (0);
+}
