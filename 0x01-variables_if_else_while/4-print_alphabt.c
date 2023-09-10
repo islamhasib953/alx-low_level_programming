f140cef0c3ce90acbc172fd66823b12829b31722
islamhasib953 <islamhasib953@gmail.com>
2023-09-10 17:33:00 +0300

    m

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print all aplhabet letters except q and e
+ *
+ * Return: Alwayes 0 (Success)
+ */
+
+int main(void)
+{
+	char ch = 'a';
+	while (ch <= 'z')
+	{
+		if (ch == 'e' || ch == 'q')
+			ch++;
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
