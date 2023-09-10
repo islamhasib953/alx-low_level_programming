bc41da0ecb0807409c6b7e084ec4be5db086d32a
islamhasib953 <islamhasib953@gmail.com>
2023-09-10 17:46:59 +0300

    m

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Decription: print 0-9 using putchar
+ *             while using int variable
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int x = 0;
+	
+	while (x <= 9)
+	{
+		putchar(x + '0');
+		x++;
+	}
+	putchar("\n");
+
+	return (0);	
+}
