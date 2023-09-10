f32411b5b5882750b15d4f9752bb84394fe9565e
islamhasib953 <islamhasib953@gmail.com>
2023-09-10 16:01:37 +0300

    a

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,7 @@
-#include <unnistd.h>
+#include <unistd.h>
 
 /**
- * main Entry point
+ * main - Entry point
  *
  * Description: print a qoute using write function
  *
@@ -13,6 +13,6 @@
 int main (void)
 {	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(1.quo.59);
+	write(1, quo, 59);
 	return (1);
 }
