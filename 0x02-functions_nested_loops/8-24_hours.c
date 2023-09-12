c6ff8df9cb0bc2efce740166cfeaba2cdcdfff97
islamhasib953 <islamhasib953@gmail.com>
2023-09-13 00:18:02 +0300

    m

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,7 +9,7 @@ void jack_bauer(void)
 	int x, y;
 	for (x = 0; x <= 23; x++)
 	{
-		for (int y = 0; y <= 59; y++)
+		for (y = 0; y <= 59; y++)
 		{
 			_putchar((x / 10) + 48);
 			_putchar((x % 10) + 48);
