943a0636bf1936c3214d3b42711da2daaaae06ae
islamhasib953 <islamhasib953@gmail.com>
2023-09-15 00:52:09 +0300

    m

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -23,9 +23,9 @@ int main(void)
 		else if (num % 3 == 0 && num % 5 ==0)
 			printf("FuzzBuzz");
 		else
-			printf("%d",num);
+			printf("%d", num);
 		if (num != 100)
-			printf(" ");
+			printf(' ');
 		else
 			printf('\n');
 	}
