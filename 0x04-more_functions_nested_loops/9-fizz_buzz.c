a5eb5579d8a8aa963c631f4511cc73a9a6c57211
islamhasib953 <islamhasib953@gmail.com>
2023-09-15 00:54:47 +0300

    m

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,18 +16,18 @@ int main(void)
 
 	for (num = 1; num <= 100; num++)
 	{
-		if (num % 3 == 0 && !(num % 5 ==0))
+		if (num % 3 == 0 && !(num % 5 == 0))
 			printf("Fizz");
-		else if (num % 5 ==0 && !(num %3 == 0))
+		else if (num % 5 == 0 && !(num % 3 == 0))
 			printf("Buzz");
-		else if (num % 3 == 0 && num % 5 ==0)
+		else if (num % 3 == 0 && num % 5 == 0)
 			printf("FuzzBuzz");
 		else
 			printf("%d", num);
 		if (num != 100)
-			printf(' ');
+			printf(" ");
 		else
-			printf('\n');
+			printf("\n");
 	}
 	return (0);
 }
