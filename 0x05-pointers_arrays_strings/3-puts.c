a424beff4157d650babaea42957ce38c13362518
islamhasib953 <islamhasib953@gmail.com>
2023-09-19 01:21:31 +0300

    m

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -10,10 +10,10 @@
 
 void _puts(char *str)
 {
-	while (*str != '\0')
-	{
-		_putchar(*str + 0);
-		str++;
-	}
-	_putchar('\n');
+    while (*str != '\0')
+    {
+        printf(*str);
+        str++;
+    }
+    _putchar('\n');
 }
