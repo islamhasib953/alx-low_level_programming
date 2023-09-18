7961b7cce12260e4a601f189c6d93f9e630ea81c
islamhasib953 <islamhasib953@gmail.com>
2023-09-19 01:34:23 +0300

    m

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * print_rev - prints revarsed string by a newline
@@ -12,9 +11,10 @@
 void print_rev(char *s)
 {
     int i = 0;
-
+    
     while (s[i])
-        i++;
+	    i++;
+
     while (i--)
     {
 	    _putchar(s[i]);
