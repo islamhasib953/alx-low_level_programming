b95b805476e608900905537d60d736cf35aadab2
islamhasib953 <islamhasib953@gmail.com>
2023-09-19 01:15:40 +0300

    m

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+i#include "main.h"
+
+/**
+ * print_rev - prints revarsed string by a newline
+ *
+ * @s: pointer tothe string print
+ *
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		i++;
+
+		while (i--)
+		{
+			_putchar(s[i]);
+		}
+		_putchar('\n');
+	}
+}
