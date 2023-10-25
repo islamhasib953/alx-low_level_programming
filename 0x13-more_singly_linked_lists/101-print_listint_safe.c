aa5325343ef620acc5819937d5d229d3c8940b6f
islamhasib953 <islamhasib953@gmail.com>
2023-10-25 20:01:03 +0300

    m

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -16,7 +16,7 @@ const listint_t **_r(const listint_t **list, size_t size, const listint_t *new)
 	const listint_t **newlist;
 	size_t i;
 
-	newlist = malloc(size - sizeof(listint_t *));
+	newlist = malloc(size * sizeof(listint_t *));
 	if (newlist == NULL)
 	{
 		free(list);
