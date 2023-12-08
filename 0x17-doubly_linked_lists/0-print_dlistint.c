20068ac86a49d579bd1326d9ca95f9c6883858c9
islamhasib953 <islamhasib953@gmail.com>
2023-12-08 22:52:51 +0200

    0x17-doubly_linked_lists

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -9,12 +9,12 @@
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t i = 0;
-
-    while (h)
-    {
-        printf("%d\n",  h->n);
-        h = h->next;
-        i++;
-    }
+	
+	while (h)
+	{
+		printf("%d\n",  h->n);
+		h = h->next;
+		i++;
+	}
 	return (i);
 }
